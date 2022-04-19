019ca1729205c9ae9c7a64aa86bfc9efead6c4b4
WoosangLee <recollect12@gmail.com>
2022-04-18 22:45:19 -0400

    use plain Unix instead of Lwt_unix

--- test/zero2prod.ml
+++ test/zero2prod.ml
@@ -1,29 +1,28 @@
 let get_free_port () = 
-  let open Lwt in
-  let socket = Lwt_unix.socket Lwt_unix.PF_INET Lwt_unix.SOCK_STREAM 0 in
-  let%lwt serverinfo = Lwt_unix.getaddrinfo "0.0.0.0" "0" [] in
+  let socket = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
+  let serverinfo = Unix.getaddrinfo "0.0.0.0" "0" [] in
   match serverinfo with
   | [] -> failwith "Failed to get serverinfo"
   | addr::_ ->
-    let%lwt () = Lwt_unix.bind socket addr.ai_addr in
-    let addrinfo2 = Lwt_unix.getsockname socket in
+    let () = Unix.bind socket addr.ai_addr in
+    let addrinfo2 = Unix.getsockname socket in
     match addrinfo2 with
     | ADDR_UNIX a -> failwith (Printf.sprintf "ADDRUNIX: %s" a)
     | ADDR_INET (_, port) ->
-      (Lwt_unix.close socket) >>= fun () ->
-      Lwt.return port
+      Unix.close socket;
+      port
 
 let get_server () =
-  let%lwt port = get_free_port () in
+  let port = get_free_port () in
   Dream.serve ~port:port Zero2prod.Routes.routes |> ignore;
-  Lwt.return port
+  port
 
 let build_addr port =
   Printf.sprintf "http://localhost:%i" port
 
 let test_lwt _ () =
   let open Lwt in
-  get_server () >>= fun port ->
+  let port = get_server () in
   let base_addr = build_addr port in
   let addr = Printf.sprintf "%s/health_check" base_addr in
   Cohttp_lwt_unix.Client.get (Uri.of_string addr) >|= fun (resp, _) ->
